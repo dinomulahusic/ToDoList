3e728d7d4824ee9afd054232b277fc599719657c
dinomulahusic <dinomulahusic@gmail.com>
2015-02-25 09:45:01 +0100

    Init

---
+++ ToDoList/Database/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ToDoList.Database
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("DatabaseConnectionString")
+        {
+
+        }
+    }
+}
